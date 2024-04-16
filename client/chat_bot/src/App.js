ceb4136f84a854a6adf1199a4d4db512b9adf3d3
Vinh Nguyen <vinh28062002@gmail.com>
2024-04-16 18:48:52 -0400

    Add UploadPDF.js

--- client/chat_bot/src/App.js
+++ client/chat_bot/src/App.js
@@ -1,25 +1,16 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import Ask from "./components/Ask";
+import UploadPDF from "./components/UploadPDF";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>PDF Uploader</h1>
+      <UploadPDF />
+      {/* <h1>Ask Question</h1>
+      <Ask /> */}
     </div>
   );
-}
+};
 
 export default App;
---
+++ client/chat_bot/src/components/UploadPdf.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const FileUpload = () => {
+  const [file, setFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleUpload = () => {
+    const formData = new FormData();
+    formData.append("pdf", file);
+
+    axios
+      .post("http://localhost:5000/upload", formData)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleUpload}>Upload</button>
+    </div>
+  );
+};
+
+export default FileUpload;
